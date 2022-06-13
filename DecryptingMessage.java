42542a97a5155dbb254df369bd687756bf773829
stan-ivanov <46340331+stan-ivanov@users.noreply.github.com>
2022-06-13 18:31:45 +0300

    first commit

--- DecryptingMessage.java
+++ DecryptingMessage.java
@@ -4,6 +4,16 @@ public class DecryptingMessage {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        int key = Integer.parseInt(scanner.nextLine());
+        int numberOfChar = Integer.parseInt(scanner.nextLine());
+        StringBuilder output = new StringBuilder();
+        for (int i = 1; i <= numberOfChar; i++) {
+
+            int sumAscii = scanner.next().charAt(0);
+            sumAscii += key;
+
+            output.append((char) sumAscii);
+        }
+        System.out.print(output);
     }
 }
